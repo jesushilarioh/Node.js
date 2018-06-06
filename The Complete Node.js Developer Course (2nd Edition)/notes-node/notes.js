d5c7aad6c5309af763b4800dc48e62e9e8ea61a9
jesushilarioh <jesushilariohernandez@gmail.com>
2018-06-06 17:53:45 -0500

    added playground and files to TCNDC (2nd Edition)

--- The Complete Node.js Developer Course (2nd Edition)/notes-node/notes.js
+++ The Complete Node.js Developer Course (2nd Edition)/notes-node/notes.js
@@ -1,7 +1,29 @@
 console.log("Starting notes.js");
 
+const fs = require("fs");
+
 const addNote = (title, body) => {
-  console.log("Adding note", title, body);
+  // console.log("Adding note", title, body);
+
+  let notes = [];
+  let note = { // Represent new note
+    title,
+    body
+  };
+
+  try {
+    let notesString = fs.readFileSync("notes-data.json");
+    notes = JSON.parse(notesString);
+  } catch (err) {
+
+  }
+
+  const duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+  }
 };
 
 const getAll = () => {
